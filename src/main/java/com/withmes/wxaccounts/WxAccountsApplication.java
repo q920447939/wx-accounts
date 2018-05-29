d3a5ac693400dcad20a2b715f1c0e663daaf5b72
lim <lim@haokukeji.com>
2018-05-29 17:07:30 +0800

    first commit

---
+++ src/main/java/com/withmes/wxaccounts/WxAccountsApplication.java
@@ -0,0 +1,12 @@
+package com.withmes.wxaccounts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WxAccountsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WxAccountsApplication.class, args);
+    }
+}
---
+++ src/test/java/com/withmes/wxaccounts/WxAccountsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.withmes.wxaccounts;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WxAccountsApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
